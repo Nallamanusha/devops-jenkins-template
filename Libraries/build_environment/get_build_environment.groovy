8c85ff999a3e7fa4ddd9478f537f4995fb1b168a
Nallam Anusha <95328179+Nallamanusha@users.noreply.github.com>
2025-05-14 12:17:39 +0530

    Update get_build_environment.groovy

--- Libraries/build_environment/get_build_environment.groovy
+++ Libraries/build_environment/get_build_environment.groovy
@@ -1,1 +1,15 @@
 
+void call() {
+    echo "Setting up build environment..."
+
+    // Set default values if not already set
+    if (!env.OS) {
+        env.OS = "linux"
+    }
+    if (!env.ARCH) {
+        env.ARCH = "amd64"
+    }
+
+    env.BUILD_ENV_TAG = "${env.OS}-${env.ARCH}"
+    echo "Build Env Tag: ${env.BUILD_ENV_TAG}"
+}
