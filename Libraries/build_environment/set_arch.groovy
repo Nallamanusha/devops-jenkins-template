9a93b03b492abbff5e2c8625b13a6704874f9265
Nallam Anusha <95328179+Nallamanusha@users.noreply.github.com>
2025-05-15 14:54:16 +0530

    Update set_arch.groovy

--- Libraries/build_environment/set_arch.groovy
+++ Libraries/build_environment/set_arch.groovy
@@ -1,1 +1,10 @@
+void call(String arch = "amd64") {
+    echo "Setting architecture..."
 
+    if (!env.ARCH) {
+        env.ARCH = arch
+        echo "Architecture set to default: ${arch}"
+    } else {
+        echo "Architecture already set: ${env.ARCH}"
+    }
+}
