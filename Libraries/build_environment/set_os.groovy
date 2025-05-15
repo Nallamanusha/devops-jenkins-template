537027ab9df5487224b71a3b82a9801b2e0f8038
Nallam Anusha <95328179+Nallamanusha@users.noreply.github.com>
2025-05-15 14:54:38 +0530

    Update set_os.groovy

--- Libraries/build_environment/set_os.groovy
+++ Libraries/build_environment/set_os.groovy
@@ -1,1 +1,11 @@
+void call(String os = "linux") {
+    echo "Setting operating system..."
+
+    if (!env.OS) {
+        env.OS = os
+        echo "OS set to default: ${os}"
+    } else {
+        echo "OS already set: ${env.OS}"
+    }
+}
 
