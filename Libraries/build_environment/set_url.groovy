e5d53bcd00eda6e4fbd4aba912e344d6820b73be
Nallam Anusha <95328179+Nallamanusha@users.noreply.github.com>
2025-05-15 14:54:58 +0530

    Update set_url.groovy

--- Libraries/build_environment/set_url.groovy
+++ Libraries/build_environment/set_url.groovy
@@ -1,1 +1,11 @@
+void call() {
+    echo "Setting URL based on environment..."
+
+    if (!env.URL) {
+        env.URL = "https://example.com/builds/${env.OS}/${env.ARCH}"
+        echo "URL set to: ${env.URL}"
+    } else {
+        echo "URL already set: ${env.URL}"
+    }
+}
 
